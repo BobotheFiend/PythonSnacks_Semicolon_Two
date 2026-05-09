faa3651bead3536e079253e887480c75614b672d
Nnamdi <cavennnamdi@gmail.com>
2026-05-09 19:56:38 +0100

    Weekend fun

--- Assignments/weekend_fun/rock_paper_scissors.py
+++ Assignments/weekend_fun/rock_paper_scissors.py
@@ -14,5 +14,5 @@ if (player_one == "rock" and player_two == "scissors") or (player_one == "scisso
 elif (player_two ==  "rocks" and player_one == "scissors") or (player_two == "scissors" and player_one == "paper") or (player_two == "paper" and player_one == "rock"):
     print(f"Player Two wins!!!\n{player_two} beats {player_one} always!!")
 
-elif (player_one ==  "rock" and player_two == "rock") or (player_one == "paper" and player_two == "paper") or (player_one == "scissors" and player_two == "scissors"):
+elif (player_one, player_two == "rock") or (player_one, player_two == "paper") or (player_one, player_two == "scissors"):
     print(f" A Tie!\n{player_one} and {player_two} ties always!!")
